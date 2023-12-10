68eee7b4088b9de1264a30dc9060fae9db8e1efe
NataTsuts <nata.tsutsman.13@gmail.com>
2023-12-10 14:47:32 +0200

    prettier

--- src/components/IconButton/styled.ts
+++ src/components/IconButton/styled.ts
@@ -2,7 +2,7 @@ import styled from "styled-components"
 import { theme } from "../../theme"
 
 export const StyledIconButton = styled.button`
-  background-color:${theme.colors.secondary};
+  background-color: ${theme.colors.secondary};
   margin: 10px;
   border: none;
   width: 53px;
